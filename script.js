d5e496d7ebdc91a95cd6280750fdcdd7d2af5ad1
Myles Dawson <mylesd@sfu.ca>
2017-08-02 21:34:36 -0700

    added footer. Made page look better at different screen sizes

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ $(document).ready(function(){
 	$(".navbar-burger").click(function() {
 		// console.log("clicked navbar-burger");
 		$(this).toggleClass("is-active");
+
+		var $navMenu = $("div.navbar-menu");
+		$navMenu.toggleClass("is-active");
 	});
 
 	$("a.has-text-white").hover(function() {
 		// console.log("hovered");
 		$(this).toggleClass("has-text-dark");
-	})
+	});
+
+	$("a.icon i.has-text-danger").hover(function() {
+		$(this).toggleClass("has-text-danger");
+		$(this).toggleClass("has-text-white");
+	});
+
+	$("#action-call-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#tiles-section").offset().top
+    }, 500);
+	});
+
 });
